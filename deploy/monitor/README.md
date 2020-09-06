47e80c985a61d0acbb5e7c3e432e436a6c420960
cuisaihang <1114313879@qq.com>
2020-09-06 18:11:57 +0800

    优化目录结构，增加prometheus部署文件

---
+++ deploy/monitor/README.md
@@ -0,0 +1,13 @@
+### 
+**注意事项**：
+1. 创建pv挂载目录
+~~~
+sudo make /tmp/data/prometheus 
+~~~
+2. 启动prometheus服务
+~~~
+make apply
+~~~
+3. 
+grafana dashboard 地址 (http://127.0.0.1:32213) - 用户名:admin 密码: prom-operator
+prometheus dashboard 地址 (http://127.0.0.1:30090)
--- plugins/wrapper/skywalking.go
+++ plugins/wrapper/skywalking.go
@@ -1,1 +1,15 @@
 package wrapper
+
+import (
+	"context"
+	"github.com/micro/go-micro/v2/server"
+)
+
+func NewLogWrapper(fn server.HandlerFunc) server.HandlerFunc {
+	return func(ctx context.Context, req server.Request, rsp interface{}) error {
+		//log.Printf("[Log Wrapper] Before serving request method: %v", req.Endpoint())
+		err := fn(ctx, req, rsp)
+		//log.Printf("[Log Wrapper] After serving request")
+		return err
+	}
+}
